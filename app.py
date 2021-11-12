6ea0e255aa7943e09cdf02a921e80b0875d24de0
HVL582613 <54099377+HVL582613@users.noreply.github.com>
2021-11-12 16:59:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -37,6 +37,11 @@ class StreamlitApp:
         
        
         if add_selectbox == 'Online':
+            MSSubClass=st.selectbox('The building class', ['1-STORY 1946 & NEWER ALL STYLES', '1-STORY 1945 & OLDER', '1-STORY W/FINISHED ATTIC ALL AGES', 
+                                                           '1-1/2 STORY - UNFINISHED ALL AGES', '1-1/2 STORY FINISHED ALL AGES', '2-STORY 1946 & NEWER',
+                                                           '2-STORY 1945 & OLDER', '2-1/2 STORY ALL AGES', 'SPLIT OR MULTI-LEVEL', 'SPLIT FOYER', 'DUPLEX - ALL STYLES AND AGES',
+                                                           '1-STORY PUD (Planned Unit Development) - 1946 & NEWER', '1-1/2 STORY PUD - ALL AGES', 
+                                                           '2-STORY PUD - 1946 & NEWER','PUD - MULTILEVEL - INCL SPLIT LEV/FOYER', '2 FAMILY CONVERSION - ALL STYLES AND AGES'])
             GrLivArea = st.number_input('Above grade (ground) living area square feet', min_value=300, max_value=6000, value=1500)
             OverallCond = st.number_input('Overall condition rating', min_value=1, max_value=9, value=5)
             OverallQual = st.number_input('Overall material and finish quality', min_value=1, max_value=10, value=5)
@@ -70,6 +75,7 @@ class StreamlitApp:
                                                                            'Adjacent to East-West Railroad'])
             BldgType=st.selectbox('Type of dwelling', ['Single-family Detached', 'Two-family Conversion; originally built as one-family dwelling', 
                                                        'Duplex', 'Townhouse End Unit', 'Townhouse Inside Unit'])
+            HouseStyle=st.selectbox('Style of dwelling', ['1Story', '1 .5Fin', '1 .5Unf', '2Story', '2 .5Fin', '2 .5Unf', 'SFoyer', 'SLvl'])
             YearBuilt = st.number_input('Original construction date', min_value=1800, max_value=2021, value=1980)
             YearRemodAdd=st.number_input('Remodel date (same as construction date if no remodeling or additions)', min_value=1800, max_value=2021, value=1980)
             RoofStyle=st.selectbox('Type of roof', ['Flat', 'Gable', 'Gabrel(Barn)', 'Hip', 'Mansard', 'Shed'])
@@ -103,46 +109,81 @@ class StreamlitApp:
             Heating = st.selectbox('Type of heating', ['Floor furnace','Gas forced warm air furnace','Gas hot water or steam heat','Gravity furnace',
                                                        'Hot water or steam heat other than gas','Wall furnace'])
             HeatingQC = st.selectbox('Heating quality and condition', ['Excellent', 'Good', 'Average/typical', 'Fair', 'Poor'])
-            Functional = st.selectbox('Home functionality rating', ['Typ', 'Min1', 'Min2', 'Mod', 'Maj1', 'Maj2', 'Sev', 'Sal'])
-            GarageType = st.selectbox('Garage location', ['2Types', 'Attchd', 'Basment', 'BuiltIn', 'CarPort', 'Detchd', 'NA'])
-            GarageCars =st.number_input('Size of garage in car capacity', min_value=0, max_value=10, value=1)
-            GarageArea = st.number_input('Size of garage in square feet', min_value=0.0, max_value=2000.0, value=1000.0)
-            TotRmsAbvGrd = st.number_input('Total rooms above grade (does not include bathrooms)', min_value=0.0, max_value=30.0, value=6.0)
-            Fireplaces = st.number_input('Number of fireplaces', min_value=0, max_value=5, value=1)
-            PoolArea = st.number_input('Pool area in square feet', min_value=0.0, max_value=1000.0, value=500.0)
-            PoolQC=st.selectbox('Pool quality', ['Gd', 'TA', 'Fa', 'NA'])
-            Fence = st.selectbox('Fence quality', ['MnPrv', 'GdWo', 'MnWw', 'NA'])
-            MSSubClass=st.selectbox('The building class', [20,30,40,45,50,60,70,75,80,85,90,120,150,160,180,190])
-            HouseStyle=st.selectbox('Style of dwelling', ['1Story', '1 .5Fin', '1 .5Unf', '2Story', '2 .5Fin', '2 .5Unf', 'SFoyer', 'SLvl'])
-            SaleCondition=st.selectbox('Condition of sale', ['Normal', 'Abnormal', 'AdjLand', 'Alloca', 'Family', 'Partial'])
-            SaleType=st.selectbox('Type of sale', ['WD', 'CWD', 'VWD', 'New', 'COD', 'Con', 'ConLw', 'ConLI', 'ConLD', 'Oth'])
-            YrSold=st.number_input('Year Sold', min_value=1950, max_value=2050, value=2010)
-            MoSold=st.selectbox('Month Sold', [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
-            MiscVal=st.number_input('$ Value of miscellaneous feature', min_value=0, max_value=15500, value=5000)
-            MiscFeature=st.selectbox('Miscellaneous feature not covered in other categories', ['Elev', 'Gar2', 'Othr', 'Shed', 'TenC', 'NA'])
-            ScreenPorch=st.number_input('Screen porch area in square feet', min_value=0, max_value=1000, value=250)
-            SsnPorch=st.number_input('Three season porch area in square feet', min_value=0, max_value=1000, value=250)
-            EnclosedPorch=st.number_input('Enclosed porch area in square feet', min_value=0, max_value=1000, value=250)
-            OpenPorchSF=st.number_input('Open porch area in square feet', min_value=0, max_value=1000, value=250)
-            WoodDeckSF=st.number_input('Wood deck area in square feet', min_value=0, max_value=1000, value=250)
-            PavedDrive=st.selectbox('Paved driveway', ['Y', 'P', 'N'])
-            GarageCond=st.selectbox('Garage condition', ['Ex', 'Gd', 'TA', 'Fa', 'Po', 'NA'])
-            GarageQual=st.selectbox('Garage quality', ['Ex', 'Gd', 'TA', 'Fa', 'Po', 'NA'])
-            GarageFinish=st.selectbox('Interior finish of the garage', ['Fin', 'RFn', 'Unf', 'NA'])
-            GarageYrBlt=st.number_input('Year garage was built', min_value=1900, max_value=2050, value=1980)
-            FireplaceQu=st.selectbox('Fireplace quality', ['Ex', 'Gd', 'TA', 'Fa', 'Po', 'NA'])
-            KitchenQual=st.selectbox('Kitchen quality', ['Ex', 'Gd', 'TA', 'Fa', 'Po', 'NA'])
-            KitchenAbvGr=st.number_input('Number of kitchens', min_value=0, max_value=10, value=2)
-            BedroomAbvGr=st.number_input('Number of bedrooms above basement level', min_value=0, max_value=20, value=4)
+            CentralAir=st.selectbox('Central air conditioning', ['No', 'Yes'])
+            Electrical=st.selectbox('Electrical system', ['Standard Circuit Breakers & Romex', 'Fuse Box over 60 AMP and all Romex wiring (Average)', 
+                                                          '60 AMP Fuse Box and mostly Romex wiring (Fair)', 
+                                                          '60 AMP Fuse Box and mostly knob & tube wiring (poor)', 'Mixed'])
+            OnestFlrSF=st.number_input('First Floor square feet', min_value=100, max_value=6000, value=400)
+            TwondFlrSF=st.number_input('Second floor square feet', min_value=0, max_value=3000, value=300)
+            LowQualFinSF=st.number_input('Low quality finished square feet (all floors)', min_value=0, max_value=1000, value=5)
             FullBath = st.number_input('Full bathrooms above grade', min_value=0, max_value=5, value=0)
             HalfBath=st.number_input('Half baths above grade', min_value=0, max_value=10, value=2)
             BsmtHalfBath=st.number_input('Basement half bathrooms', min_value=0, max_value=10, value=1)
             BsmtFullBath=st.number_input('Basement full bathrooms', min_value=0, max_value=10, value=1)
-            LowQualFinSF=st.number_input('Low quality finished square feet (all floors)', min_value=0, max_value=1000, value=5)
-            TwondFlrSF=st.number_input('Second floor square feet', min_value=0, max_value=3000, value=300)
-            OnestFlrSF=st.number_input('First Floor square feet', min_value=100, max_value=6000, value=400)
-            Electrical=st.selectbox('Electrical system', ['SBrkr', 'FuseA', 'FuseF', 'FuseP', 'Mix'])
-            CentralAir=st.selectbox('Central air conditioning', ['No', 'Yes'])
+            KitchenAbvGr=st.number_input('Number of kitchens', min_value=0, max_value=10, value=2)
+            KitchenQual=st.selectbox('Kitchen quality', ['Excellent', 'Good', 'Average/typical', 'Fair', 'Poor'])
+            TotRmsAbvGrd = st.number_input('Total rooms above grade (does not include bathrooms)', min_value=0, max_value=30, value=6)
+            BedroomAbvGr=st.number_input('Number of bedrooms above basement level', min_value=0, max_value=20, value=4)
+            Functional = st.selectbox('Home functionality rating', ['Typical Functionality', 'Minor Deductions 1', 'Minor Deductions 2', 'Moderate Deductions', 
+                                                                    'Major Deductions 1', 'Major Deductions 2', 'Severly damaged', 'Salvage only'])
+            Fireplaces = st.number_input('Number of fireplaces(0-5)', min_value=0, max_value=5, value=1)
+            FireplaceQu=st.selectbox('Fireplace quality', ['Excellent - Exceptional Masonry Fireplace', 'Good - Masonry Fireplace in main level', 
+                                                           'Average - Prefabricated Fireplace in main living area or Masonry Fireplace in basement', 
+                                                           'Fair - Prefabricated Fireplace in basement', 
+                                                           'Poor - Ben Franklin Stove', 'No fireplace'])
+            GarageType = st.selectbox('Garage location', ['More than one type of garage', 'Attached to home', 'Basement garage', 
+                                                          'Built-In (Garage part of house - typically has room above garage)', 'Car Port', 'Detached from home', 'No garage'])
+            GarageCond=st.selectbox('Garage condition', ['Excellent', 'Good', 'Average/typical', 'Fair', 'Poor', 'No garage'])
+            GarageQual=st.selectbox('Garage quality', ['Excellent', 'Good', 'Average/typical', 'Fair', 'Poor', 'No garage'])
+            GarageFinish=st.selectbox('Interior finish of the garage', ['Finished', 'Rough Finished', 'Unfinished', 'No garage'])
+            GarageYrBlt=st.number_input('Year garage was built', min_value=1900, max_value=2050, value=1980)
+            GarageCars =st.number_input('Size of garage in car capacity', min_value=0, max_value=10, value=1)
+            GarageArea = st.number_input('Size of garage in square feet', min_value=0.0, max_value=2000.0, value=1000.0)
+            PavedDrive=st.selectbox('Paved driveway', ['Paved', 'Partial pavement', 'Dirt/gravel'])
+            WoodDeckSF=st.number_input('Wood deck area in square feet', min_value=0, max_value=1000, value=250)
+            OpenPorchSF=st.number_input('Open porch area in square feet', min_value=0, max_value=1000, value=250)
+            EnclosedPorch=st.number_input('Enclosed porch area in square feet', min_value=0, max_value=1000, value=250)
+            ScreenPorch=st.number_input('Screen porch area in square feet', min_value=0, max_value=1000, value=250)
+            SsnPorch=st.number_input('Three season porch area in square feet', min_value=0, max_value=1000, value=250)
+            PoolArea = st.number_input('Pool area in square feet', min_value=0.0, max_value=1000.0, value=500.0)
+            PoolQC=st.selectbox('Pool quality', ['Excellent', 'Good', 'Average/typical', 'Fair', 'No pool'])
+            Fence = st.selectbox('Fence quality', ['Good Privacy', 'Minimum Privacy', 'Good Wood', 'Minimum Wood/Wire', 'No fence'])
+            MiscFeature=st.selectbox('Miscellaneous feature not covered in other categories', ['Elevator', '2nd Garage (if not described in garage section)', 
+                                                                                               'Other', 'Shed(over 100 SF)', 'Tennis Court', 'None'])
+            MiscVal=st.number_input('$ Value of miscellaneous feature', min_value=0, max_value=15500, value=5000)
+            YrSold=st.number_input('Year Sold', min_value=1950, max_value=2050, value=2010)
+            MoSold=st.selectbox('Month Sold', [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+            SaleType=st.selectbox('Type of sale', ['Warranty Deed - Conventional', 'Warranty Deed - Cash', 'Warranty Deed - VA Loan', 'Home just constructed and sold', 
+                                                   'Court Officer Deed/Estate', 'Contract 15% Down payment regular terms', 'Contract Low Down payment and low interest', 
+                                                   'Contract Low Interest', 'Contract Low Down', 'Other'])
+            SaleCondition=st.selectbox('Condition of sale', ['Normal', 'Abnormal Sale -  trade, foreclosure, short sale', 
+                                                             'Adjoining Land Purchase', 'Allocation - two linked properties with separate deeds, typically condo with a garage unit', 
+                                                             'Sale between family members', 'Home was not completed when last assessed (associated with New Homes)'])
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+           
+            
+            
+           
+            
+            
          
             
             
